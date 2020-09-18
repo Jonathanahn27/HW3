038643d3946792f0f985d2de21d24f1c097173be
Jonathanahn27 <Jonathanahn27@gmail.com>
2020-09-18 19:22:54 +0000

    changed //

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def digit_sum(n):
   if n<= 0:
     return 0
   else:
-    return n%10+digit_sum((n/10)-(n%10*.1))
+    return n%10+digit_sum(n//10)
 
 n = int(input("Enter an int: "))
 s = int(digit_sum(n))
